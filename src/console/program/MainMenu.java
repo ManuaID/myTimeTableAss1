3f2653492eb94322342f46742b333297fd785eb7
ManuaID <matthieu.kwong@gmail.com>
2026-07-29 15:27:34 +1000

    Reimplmeneted print function and deleted certain files

---
+++ src/console/program/MainMenu.java
@@ -0,0 +1,72 @@
+package console.program;
+
+import java.util.Scanner;
+
+/**
+ * The Restaurant class provides the functionality needed to manage orders and checkout.
+ */
+public class MainMenu {
+
+    private String name;
+    
+    public MainMenu(String name) {
+    	this.name = name;
+    }
+    
+    public String getName() {
+    	return this.name;
+    }
+    
+	/**
+     * The method to operate the restaurant.
+     */
+    public void run() {
+    	boolean exit = false;
+		String input = "";
+		while(exit) {
+			printMenu(name);
+
+			input = readUserInput();
+
+			switch (input) {
+				case "1":
+					System.out.println("Search function to be coded");
+					break;
+				case "2":
+					System.out.println("Enroll function to be coded");
+					break;
+				case "3":
+					System.out.println("Implement Withdraw from course");
+					break;
+				default:
+					System.out.println("Please input a number between 1-4");
+					throw new AssertionError();
+			}
+		}
+
+    }
+    
+	/**
+     * The utility method to print menu options.
+     */
+	public static void printMenu(String name){
+		String banner = new String(new char[50]).replace('\u0000', '-');
+
+		System.out.println("Welcome to " + name + "!");
+		System.out.println(banner);
+		System.out.println("> Select from main menu");
+		System.out.println(banner);
+		System.out.println("  1) Search by keyword to enroll");
+		System.out.println("  2) Show my enrolled courses");
+		System.out.println("  3) Withdraw from a course");
+		System.out.println("  4) Exit");
+	}
+	
+	/**
+     * The utility method to read user input.
+     */
+    public static String readUserInput() {
+	    Scanner sc = new Scanner(System.in);
+	    return sc.nextLine();
+	}
+}
