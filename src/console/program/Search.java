35240f22bc555c3b964bd446d7c99ca9cc220a14
ManuaID <matthieu.kwong@gmail.com>
2026-07-30 21:28:12 +1000

    fixed search function issue, where it didn't search without having a exact spelling and case of the course on the csv

--- src/console/program/Search.java
+++ src/console/program/Search.java
@@ -1,11 +1,13 @@
 package console.program;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Search {
 
     Data data = new Data();
     ArrayList<data_record> datas = new ArrayList<>();
+    Scanner scnr = new Scanner(System.in);
 
     public Search() {
         datas = data.getData("course-1.csv");
@@ -19,7 +21,9 @@ public class Search {
         while(!courseExists) {
 
             for (data_record data : datas) {
-                if (data.course_name.contains(input)) {
+                String courseName = data.course_name.toLowerCase();
+                String inputLower = input.toLowerCase();
+                if (courseName.contains(inputLower)) {
                     search_results.add(data.course_name);
                     courseExists = true;
                 }
@@ -32,9 +36,26 @@ public class Search {
     public void printList(String input) {
         ArrayList<String> results = getCourseList(input);
 
+        String banner = new String(new char[50]).replace('\u0000', '-');   
+
+        System.out.println(banner);        
+        System.out.println("Select from matching list");
+        System.out.println(banner);
+
         for(int i = 0; i < results.size(); ++i) {
-            System.out.printf("  %d) %s", i + 1, results.get(i));
+            System.out.printf("  %d) %s\n", i + 1, results.get(i));
         }
-        System.out.println("\n");
+
+        System.out.println("Please select: ");
+        Integer desiredCourse = scnr.nextInt();
+
+        if(desiredCourse >= 1 && desiredCourse <= results.size()) {
+            /* 
+                Enroll in a course if the student don't have enough credit for that 
+                semester and don't let them enroll if they are overloading or something
+
+                Add what ever enrolled to a csv maybe a db? check on requirement
+            */
+        }
     }
 }
